c53540e00db383f2bbd38abdf459f6564905345c
Doladun1 <davae123@gmail.com>
2025-09-19 08:56:34 -0400

    Use struct instead of global

--- hello_world.c
+++ hello_world.c
@@ -4,40 +4,64 @@
  *  Created on: Sep 10, 2025
  *      Author: davae
  */
+
+#include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #define INT8_MAX		127
 
-//Create queues for each type of interr
-int track[INT8_MAX];
-static int track_idx;
-static int tail_idx;
+
+// I dont want it global anymore I want an object that takes args and has private attributes
+
+typedef struct Track{
+	int arr[INT8_MAX];
+	int idx;
+	void *ptr;
+	int *ptr2;
+}Track;
 
 //Add function to fill queue based on interr
-void fill(int var){
-	track[track_idx++] = var;
+void fill(int var, Track *track){
+	printf("%s : var %d\n",__func__,var);
+	track->arr[track->idx++] = var;
 }
 
 //Empty queues after interr 
-int empty(){
+int empty(Track *track){
+	int buf;
 	printf("%s ",__func__);
-	return track[tail_idx++];
+
+	buf = track->idx ? track->arr[--track->idx] : track->arr[track->idx] ;	
+	return buf;
 }
 
 int main(){
 	printf("Input Value:\n");
-	fflush(stdout);	
+	fflush(stdout);
 	int var =0;
-	
-	while(var != -999) {
+	Track *track = (Track *) calloc(2,sizeof(Track));
+
+/*	while(var != -999) {
 		scanf("%d",&var);
-		fill(var);
+		fill(var,track);
 		}
-	
-	printf("Queue: \n");
-	while(tail_idx<track_idx){
-		printf("Val: %d\n", empty());
+*/
+	printf("Stack: \n");
+	while(track->idx>0){
+		printf("Val: %d\n", empty(track));
 	}
+
+	fill(100,track);
+	printf("idx:%d\n",track->idx);
+	printf("Val: %d\n", empty(track));
+	fill(100,track);
+
+	while(track->idx>0){
+		printf("Val: %d\n", empty(track));
+	}	
+	printf("idx:%d\n",track->idx);
+	if (++track == NULL)	printf("Nothing there\n");
+
 	return 0;
 }
